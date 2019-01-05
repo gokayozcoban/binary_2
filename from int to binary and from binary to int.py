62618dc31dee4dbd2fa502b324ac9956463c4693
gokayozcoban <43710003+gokayozcoban@users.noreply.github.com>
2019-01-05 21:33:17 +0200

    binary 2

---
+++ from int to binary and from binary to int.py
@@ -0,0 +1,10 @@
+
+# from integer to binary and from binary to integer
+
+veri = int(input("Sayı girin: "))
+if veri == veri:
+    print(bin(veri)[2:])
+    
+veri2 = input("Kodu Girin: ")
+if veri2 == veri2:
+    print(int(veri2,2))
